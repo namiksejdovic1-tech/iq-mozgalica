12208162bfca3f21c160b07bae850903ab91be7a
namiksejdovic1-tech <namik.sejdovic1@gmail.com>
2026-01-03 23:40:57 +0100

    Create games.js

---
+++ netlify/functions/games.js
@@ -0,0 +1,48 @@
+```javascript
+// Netlify Serverless Function: Get All Games
+// Endpoint: /.netlify/functions/games
+
+const gameGenerator = require('../../engine/game-generator');
+
+exports.handler = async (event, context) => {
+    // Handle CORS
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Content-Type': 'application/json'
+    };
+
+    // Handle preflight
+    if (event.httpMethod === 'OPTIONS') {
+        return {
+            statusCode: 200,
+            headers,
+            body: ''
+        };
+    }
+
+    if (event.httpMethod === 'GET') {
+        try {
+            const games = await gameGenerator.getAllGames();
+            return {
+                statusCode: 200,
+                headers,
+                body: JSON.stringify(games)
+            };
+        } catch (error) {
+            return {
+                statusCode: 500,
+                headers,
+                body: JSON.stringify({ error: error.message })
+            };
+        }
+    }
+
+    return {
+        statusCode: 405,
+        headers,
+        body: JSON.stringify({ error: 'Method not allowed' })
+    };
+};
+```
